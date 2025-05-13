1d1c5b3e67d01442ed0654371381600456437f3b
sibel <sibel@SIBEL_PC>
2025-05-13 11:39:08 +0300

    Add project files.

---
+++ ForPratik_1/Program.cs
@@ -0,0 +1,50 @@
+﻿for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Kendime inaniyorum, ben bu yazilim isini yaparim!");
+}
+
+
+
+for (int i = 1; i < 20; i++)
+{
+    Console.WriteLine(i); 
+}
+
+
+for (int i = 2; i < 20; i+= 2)
+{
+    Console.WriteLine(i);
+}
+
+{
+    int toplam = 0;
+
+    for (int i = 50; i <= 150; i++)
+{
+        toplam += i;
+}
+    Console.WriteLine("50 ile 150 arasindaki sayilarin toplami= " + toplam);
+
+}
+
+{
+    int toplam = 0;
+
+    for (int i = 2; i <= 120; i += 2)
+    {
+        toplam += i;
+    }
+    Console.WriteLine("1 ile 120 arasindaki cift sayilarin toplami= " + toplam);
+
+}
+
+{
+    int toplam = 0;
+
+    for (int i = 1; i <= 119; i += 2)
+    {
+        toplam += i;
+    }
+    Console.WriteLine("1 ile 120 arasındaki tek sayilarin toplami= " + toplam);
+}
+
